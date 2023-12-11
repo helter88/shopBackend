eb86261f6fa176810c6e07c01f33ac1c91ad4ffe
artur-helta <artur.helta@onet.pl>
2023-12-11 17:15:50 +0100

    add Pageable for getProducts method

--- src/main/java/com/artur/shop/product/service/ProductService.java
+++ src/main/java/com/artur/shop/product/service/ProductService.java
@@ -3,15 +3,16 @@ package com.artur.shop.product.service;
 import com.artur.shop.product.model.Product;
 import com.artur.shop.product.repository.ProdutRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProdutRepository produtRepository;
-    public List<Product> getProducts(){
-        return produtRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable){
+        return produtRepository.findAll(pageable);
     }
 }
